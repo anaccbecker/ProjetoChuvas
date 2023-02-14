1571e5efe0b843e6b2b4ef82d2c484bcf6e852c8
Ana Becker <anacccbecker@gmail.com>
2023-02-14 17:24:00 -0300

    2023-02-14

--- src/[Scatterplot] Dispersão.py
+++ src/[Scatterplot] Dispersão.py
@@ -7,9 +7,7 @@ Created on Mon Feb  6 10:22:28 2023
 """
 
 
-def disp_plot (df_path, x, y):
-    df = pd.read_csv(df_path).pivot(index=['time','codigo'],columns='variable',values='value')
-    df.reset_index(inplace=True)
+def disp_plot (df, x, y, freq):
     for i in range(0,len(sim)):
             df_est = df[df['codigo']== sim.codigo[i]]
             vmax = max(np.max(df_est[x]),np.max(df_est[y]))+20
@@ -18,8 +16,8 @@ def disp_plot (df_path, x, y):
                           y=y, 
                           title = f'Estação {str(sim.codigo[i])} - {str(sim.nome[i])}',
                           labels = {
-                              "x": f"Chuva horária {x} (mm)",
-                              "y": f"Chuva horária {y} (mm)"},
+                              "x": f"Chuva {freq} {x} (mm)",
+                              "y": f"Chuva {freq} {y} (mm)"},
                          template = "plotly_white")
             fig.add_shape(type = 'line',
                           x0 = 0,
@@ -30,26 +28,32 @@ def disp_plot (df_path, x, y):
                           xref = 'x',
                           yref = 'y'
                           )
-            if not os.path.isdir(f'html/[Scatterplot] Dispersão horária ({x}-{y})'):
-                os.makedirs(f'html/[Scatterplot] Dispersão horária ({x}-{y})')
-            fig.write_html(f'html/[Scatterplot] Dispersão horária ({x}-{y})/{sim.codigo[i]}.html')
+            if not os.path.isdir(f'html/[Scatterplot] Dispersão {freq} ({x}-{y})'):
+                os.makedirs(f'html/[Scatterplot] Dispersão {freq} ({x}-{y})')
+            fig.write_html(f'html/[Scatterplot] Dispersão {freq} ({x}-{y})/{sim.codigo[i]}.html')
 
 
 # nomes: 'CPC', 'Simepar', 'gsmap_now', 'gsmap_nrt'
 
 # Dispersões horárias
-disp_plot('csv/[H] s j.csv', 'Simepar','gsmap_now')
-disp_plot('csv/[H] s j.csv', 'Simepar','gsmap_nrt')
-disp_plot('csv/[H] s j.csv', 'gsmap_now','gsmap_nrt')
+freq = 'horária'
+df = pd.read_csv('csv/[H] s j.csv').pivot(index=['time','codigo'],columns='variable',values='value').reset_index()
+disp_plot(df, 'Simepar','gsmap_now', freq)
+disp_plot(df, 'Simepar','gsmap_nrt', freq)
+disp_plot(df, 'gsmap_now','gsmap_nrt', freq)
 
 # Dispersões diárias
-disp_plot('csv/[D] s j c.csv', 'Simepar','gsmap_now')
-disp_plot('csv/[D] s j c.csv', 'Simepar','gsmap_nrt')
-disp_plot('csv/[D] s j c.csv', 'gsmap_now','gsmap_nrt')   
-disp_plot('csv/[D] s j c.csv', 'Simepar','CPC')
+freq = 'diária'
+df = pd.read_csv('csv/[D] s j c.csv').pivot(index=['time','codigo'],columns='variable',values='value').reset_index()
+disp_plot(df, 'Simepar','gsmap_now', freq)
+disp_plot(df, 'Simepar','gsmap_nrt', freq)
+disp_plot(df, 'gsmap_now','gsmap_nrt', freq)   
+disp_plot(df, 'Simepar','CPC', freq)
     
 # Dispersões mensais
-disp_plot('csv/[M] s j c.csv', 'Simepar','gsmap_now')
-disp_plot('csv/[M] s j c.csv', 'Simepar','gsmap_nrt')
-disp_plot('csv/[M] s j c.csv', 'gsmap_now','gsmap_nrt')   
-disp_plot('csv/[M] s j c.csv', 'Simepar','CPC')
+freq = 'mensal'
+df = pd.read_csv('csv/[M] s j c.csv').pivot(index=['time','codigo'],columns='variable',values='value').reset_index()
+disp_plot(df, 'Simepar','gsmap_now', freq)
+disp_plot(df, 'Simepar','gsmap_nrt', freq)
+disp_plot(df, 'gsmap_now','gsmap_nrt', freq)   
+disp_plot(df, 'Simepar','CPC', freq)
